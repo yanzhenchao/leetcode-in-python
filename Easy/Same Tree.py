a661f6c3cc5e253d278bd7d0a0b370480e5f1671
Zhenchao Yan <yanzhenchao@gmail.com>
2015-02-13 18:15:04 -0500

    Create Same Tree.py

---
+++ Easy/Same Tree.py
@@ -0,0 +1,12 @@
+#1 Use Self.isSameTree for iteration
+
+class Solution:
+    # @param p, a tree node
+    # @param q, a tree node
+    # @return a boolean
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return False
